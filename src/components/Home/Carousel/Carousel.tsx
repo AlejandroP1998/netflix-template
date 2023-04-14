e831909e20cbbe5fe3c93c8eceefe1b98360f26c
Josue Alejandro Perez <1998japb@gmail.com>
2023-04-13 19:23:54 -0600

    carousel de imagenes autoplay

--- src/components/Home/Carousel/Carousel.tsx
+++ src/components/Home/Carousel/Carousel.tsx
@@ -1,8 +1,51 @@
-const Carousel = (props:any) => {
+import { useEffect, useState } from "react";
+
+interface Props {
+  images: string[]
+  autoplay?: boolean
+}
+
+const Carousel = (props: Props) => {
+
+
+  const [selectedIndex, setSelectedIndex] = useState(0)
+  const [selectedImage, setSelectedImage] = useState(props.images[0])
+  const [loaded, setLoaded] = useState(false)
+
+  useEffect(() => {
+    if (props.autoplay) {
+      const interval = setInterval(() => {
+        selectNewImage(selectedIndex, props.images)
+      }, 4000)
+      return () => clearInterval(interval)
+    }
+  })
+
+  const selectNewImage = (index: number, images: string[], next = true) => {
+    setLoaded(false)
+    setTimeout(() => {
+      const condition = next ? selectedIndex < images.length - 1 : selectedIndex > 0
+      const nextIndex =
+        next ?
+          condition ? selectedIndex + 1 : 0
+          : condition ? selectedIndex - 1 : images.length - 1
+      setSelectedImage(images[nextIndex])
+      setSelectedIndex(nextIndex)
+    }, 500)
+  }
+
+  const prev = () => {
+    selectNewImage(selectedIndex, props.images, false)
+  }
+
+  const next = () => {
+    selectNewImage(selectedIndex, props.images)
+  }
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <img src={`${selectedImage}`} alt="carousel de estrenos" className={loaded ? "loaded" : ""} onLoad={() => { setLoaded(true) }} />
+    </>
   )
 }
 
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,8 +1,20 @@
+import Carousel from "./Carousel/Carousel";
+import './Home.scss'
+
+
 const Home = () => {
+
+  const images = ['https://palomaynacho.com/wp-content/uploads/2022/09/3-star-trek-eliminada-del-calendario-de-paramount.jpg',
+    'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhBu7wIXWRupyVZpBKzOJ6RuV4yfSl9bTJwiewTRZjBsljPqR_ojmrEJUOATXvjptsDzwI_rReXrC4PlngBxnMUhKPyC5E-nNFXSy2JE7BvEEKsqSznCMTEBT22stWtRSGNxgfNfQa_JrhNULktehQrel5T_4y7WpkYNfzyiFRkD8XzLUekOJixqZEs/s1920/Presencias_ViX%20Plus.jpg',
+    'https://cinefilosoficial.com/wp-content/uploads/2021/10/dune-2.jpg',
+    'https://culturageek.com.ar/wp-content/uploads/2022/12/2a9xr09hclksa6wyhk0vn6lj9r9ud5niq5h1ix0m.jpeg']
+
   return (
-    <div>
-      
-    </div>
+    <section className="home">
+      <div className="carousel">
+        <Carousel images={images} autoplay={true} />
+      </div>
+    </section>
   )
 }
 
